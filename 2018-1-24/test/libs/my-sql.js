17b8ac955dd6dfb3c6d1b28e03594799491edc88
youngWzx <735505373@qq.com>
2018-02-02 17:57:31 +0800

    'update'

--- 2018-1-24/test/libs/my-sql.js
+++ 2018-1-24/test/libs/my-sql.js
@@ -1,11 +1,12 @@
 const mysql = require('koa-mysql');
 function createPool(config){
   const db = mysql.createPool(config);
-  const _db = db;
+  const _query = db.query.bind(db);
   db.query = function(sql){
     //const fn = ;
     return new Promise((resolve, reject)=>{
-      _db.query(sql)((err,data)=>{
+      _query(sql)((err,data)=>{
+        console.log(data);
         if (err) {
           reject(err)
         } else {
--- 2018-1-24/test/server.js
+++ 2018-1-24/test/server.js
@@ -1,4 +1,14 @@
 const koa = require('koa');
 const mysql = require('./libs/my-sql');
 
+const db = mysql.createPool({host:'localhost',user:'root',password:'123456',port:3306,database:'an_ju_ke'});
+
 const server = new koa();
+
+server.listen(8080);
+
+server.use(async (ctx, next)=>{
+  let data = await db.query('SELECT * FROM house_table');
+
+  console.log(data);
+})
